78795ffd2b02a00568f552e92793c06176ff9ca2
AforAmpere <49594382+AforAmpere@users.noreply.github.com>
2019-09-25 00:07:11 -0400

    Update StrangeSequence#1(M).py

--- StrangeSequence#1/StrangeSequence#1(M).py
+++ StrangeSequence#1/StrangeSequence#1(M).py
@@ -1,7 +1,19 @@
-a,t,n=1,[1],0;from itertools import*
-while n<100000:
- print(a);q=set(t)
- if a%4!=0 and((int(str(a)[0])+a)not in q):a+=int(str(a)[0])
- elif a%4==0 and a/2 not in q:a=int(a/2)
- else:a=next(filterfalse(q.__contains__,count(a)))
- t+=[a];n+= 1
+a,qtotal,n=1,{1},0;import time;start=time.time()
+from itertools import*
+while n < 100000000:
+        #print(a)
+        #qtotal = set(total)
+        if a%4 != 0 and ((int(str(a)[0])+a) not in qtotal):
+                a = a + int(str(a)[0])
+        elif a%4 == 0 and a/2 not in qtotal:
+                a = int(a/2)
+        else:
+                a = next(filterfalse(qtotal.__contains__, count(a)))
+        n+=1;qtotal.add(a)
+        if n%200000 == 0:
+                print ("At ",n)
+                print ("Total time: ",str(time.time()-start)[:5])
+                print ("Rate: ",str(int(float(n)/(time.time()-start))))
+for a in range(1,60000000):
+        if a not in qtotal:
+               print (a)
