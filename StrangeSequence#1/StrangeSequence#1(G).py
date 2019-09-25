ab1dfa1bb66aab922ddc0b7f3db174f8d70d3b0f
AforAmpere <49594382+AforAmpere@users.noreply.github.com>
2019-09-25 00:06:41 -0400

    Update StrangeSequence#1(G).py

--- StrangeSequence#1/StrangeSequence#1(G).py
+++ StrangeSequence#1/StrangeSequence#1(G).py
@@ -2,6 +2,6 @@ a,t,n=1,[1],0;from itertools import*
 while 1:
  print(a)
  if a%4!=0 and int(str(a)[0])+a not in t:a+=int(str(a)[0])
- elif a%4==0 and a/2 not in t:a=int(a/2)
+ elif a%4==0 and a/2 not in t:a=a//2
  else:a=next(filterfalse(t.__contains__,count(a)))
  t+=[a]
